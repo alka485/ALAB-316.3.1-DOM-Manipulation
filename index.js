0b7fb6a011070a19c562e4322504690e4a984756
Alka Sah <alkasah485@gmail.com>
2024-05-17 13:29:14 -0700

    Part 3  completed

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 //Select and cache the <main> element in a variable named mainEl.
 const mainEl = document.getElementsByTagName('main');
 console.log(mainEl);
+
+//Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+const subMenuEl = document.getElementById('sub-menu');
+
+//Set the height subMenuEl element to be "100%".
+subMenuEl.style.height ="100%";
+
+//Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+//Add the class of flex-around to the subMenuEl element.
+subMenuEl.classList.add('flex-around');
+
+//Set the CSS position property of subMenuEl to the value of absolute.
+subMenuEl.style.position = "absolute";
+
+//Set the CSS top property of subMenuEl to the value of 0.
+subMenuEl.style.top = "0";
+
+
 //Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 mainEl[0].style.backgroundColor = "var(--main-bg)";
 
